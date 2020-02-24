c988a385bbb428011c81d81549cbb2ee29acfa62
myhency <hency.yeo@gmail.com>
2020-02-24 19:25:59 +0900

    feature - add line graph

--- src/OverviewLayout.js
+++ src/OverviewLayout.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import { Button, Container, Divider, Grid, Header, Image, Menu, Segment, Icon, Table, Label } from 'semantic-ui-react';
 import logo from './hyundai-autoever-ci.png';
+import { Line } from 'react-chartjs-2';
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
+
+const options = {
+    maintainAspectRatio: false
+}
 
 const OverviewLayout = () => (
     <div>
@@ -87,7 +119,10 @@ const OverviewLayout = () => (
 
             <Grid.Row>
                 <Grid.Column>
-
+                    <div style={{ marginLeft: '2em', marginRight: '2em' }}>
+                        <h2>Line Example</h2>
+                        <Line data={data} height={30} option={options} />
+                    </div>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
