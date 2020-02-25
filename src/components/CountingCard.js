56bc62b7392b2321d9c74b878a09afc5908f9467
james <hency.yeo@gmail.com>
2020-02-25 22:41:36 +0900

    chore - remove Container component

--- src/components/CountingCard.js
+++ src/components/CountingCard.js
@@ -5,14 +5,13 @@ const CountingCard = (props) => {
     let textColor = props.count > 0 ? 'red' : 'green';
 
     return (
-    <Segment placeholder style={{ marginLeft: '2em', "justify-content": 'start' }}>
-        <Container text>
-            <Header as='h1'>{props.title}</Header>            
-        </Container>
-        <Container textAlign='center'>
-            <p style={{ fontSize: '120px', fontWeight: 'bold', color: textColor }}>{props.count}</p>
-        </Container>
-    </Segment>
-)};
+        <Segment placeholder style={{ marginLeft: '2em', justifyContent: 'start' }}>
+            <Header as='h1'>{props.title}</Header>
+            <Container textAlign='center'>
+                <p style={{ fontSize: '120px', fontWeight: 'bold', color: textColor }}>{props.count}</p>
+            </Container>
+        </Segment>
+    )
+};
 
 export default CountingCard;
