de086d06e497b515842be139aa2090090c60843a
james <hency.yeo@gmail.com>
2020-02-27 21:33:23 +0900

    style - remove unused import

--- src/pages/SiteList.js
+++ src/pages/SiteList.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Grid, Segment } from 'semantic-ui-react';
 import InstanceHealthCheckView from '../views/InstanceHealthCheckView';
-import ApiCallsView from '../views/ApiCallsView';
-import ServiceStatusSliderView from '../views/ServiceStatusSliderView';
-import ErrorCountingView from '../views/ErrorCountingView';
 
 const SiteList = () => (
     <InstanceHealthCheckView />
-
 );
 
 export default SiteList;
