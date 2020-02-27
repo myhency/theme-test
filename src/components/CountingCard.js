8a8d4cf1a454f31d1cf2598934bb0f59632c9bfb
james <hency.yeo@gmail.com>
2020-02-27 18:41:47 +0900

    chore - fix info text color

--- src/components/CountingCard.js
+++ src/components/CountingCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Segment, Container, Header } from 'semantic-ui-react';
 
 const getTextColor = (props) => {
-    if(props.type == 'info') return 'black';
+    if(props.type == 'info') return '#0A1654';
     return props.count > 0 ? 'red' : 'green';
 }
 
