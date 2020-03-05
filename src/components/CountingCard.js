d6464ecae0f4da853744d97a6a65ca3a34ef21c8
james <hency.yeo@gmail.com>
2020-03-05 15:51:57 +0900

    feat - add period label

--- src/components/CountingCard.js
+++ src/components/CountingCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment, Header } from 'semantic-ui-react';
+import { Segment, Header, Card, Label } from 'semantic-ui-react';
 
 const getTextColor = (props) => {
     if (props.type == 'info') return '#0A1654';
@@ -11,15 +11,22 @@ const CountingCard = (props) => {
     let textColor = getTextColor(props);
 
     return (
-        <Segment style={{ justifyContent: 'start' }}>
-            <Header as='h1'>{props.title}</Header>
-            <p style={{
-                fontSize: '100px',
-                fontWeight: 'bold',
-                color: textColor,
-                textAlign: 'center'
-            }}>{props.count}</p>
-        </Segment>
+        <Card>
+            <Header as='h1' style={{ marginTop: '14px', marginLeft: '14px' }}>{props.title} <Label attached='top right' size='mini'>60m
+                </Label></Header>
+            <Card.Content>
+                <Card.Description>
+                    <p style={{
+                        fontSize: '100px',
+                        fontWeight: 'bold',
+                        color: textColor,
+                        textAlign: 'center'
+                    }}>
+                        {props.count}
+                    </p>
+                </Card.Description>
+            </Card.Content>
+        </Card>
     )
 };
 
