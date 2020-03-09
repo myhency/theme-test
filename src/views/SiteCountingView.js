74ff8a44b17a2263d20d8062d580da08038f9661
myhency <hency.yeo@gmail.com>
2020-03-09 16:24:54 +0900

    feat - add api calls function for site counting - calls every 3 secs

--- src/views/SiteCountingView.js
+++ src/views/SiteCountingView.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CountingCard from '../components/CountingCard';
+import Fetch from '../utils/Fetch';
+import axios from 'axios';
 
-const SiteCountingView = () => (
-    <CountingCard title={'Sites'} type='info' count={1} />
-);
+const url = `/api/sites/count`;
+
+class SiteCountingView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    componentDidMount() {
+        setInterval(this.getSiteCount, 3000);
+    }
+
+    getSiteCount = () => {
+        try {
+            return axios.get(url).then(response => {
+                this.setState({
+                    count: response.data.result
+                })
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        // Fetch.GET(url)
+        //     .then((res) => {
+        //         res.json()
+        //     })
+        //     .then(response => {
+        //         console.log(response)
+        //         // this.setState({
+        //         //     count: res.result
+        //         // })
+        //     });
+    }
+
+    render() {
+        const { count } = this.state;
+        return (
+            <CountingCard title={'Sites'} type='info' count={count} />
+        );
+    }
+}
 
 export default SiteCountingView;
