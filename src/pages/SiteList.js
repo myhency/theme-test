fb8166882a8cb37e01716f2e1a243b2aa764a16a
myhency <hency.yeo@gmail.com>
2020-03-12 08:32:34 +0900

    feat - add backend

--- src/pages/SiteList.js
+++ src/pages/SiteList.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { 
-    Button, 
-    Form, 
-    Header, 
-    Modal, 
+import {
+    Button,
+    Form,
+    Header,
+    Modal,
     Grid,
-    Menu, 
-    Icon, 
-    Search, 
-    Card, 
-    Image, 
-    Dropdown, 
-    Label, 
-    Divider } from 'semantic-ui-react';
+    Menu,
+    Icon,
+    Search,
+    Card,
+    Image,
+    Dropdown,
+    Label,
+    Divider
+} from 'semantic-ui-react';
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
-import siteData from '../assets/data/SiteData.json';
+// import siteData from '../assets/data/SiteData.json';
 import Gallery from '../utils/Gallery';
+import axios from 'axios';
 
 const fileSelector = document.createElement('input');
 
@@ -33,15 +35,32 @@ class SiteList extends Component {
             open: false,
             addSiteModalOpen: false,
             modifySiteModalOpen: false,
-            totalCount: 3,
+            totalCount: 0,
             modifiedCard: {
                 name: '',
                 openDate: ''
-            }
+            },
+            siteList: [{}]
         };
 
         fileSelector.setAttribute('type', 'file');
         fileSelector.setAttribute('multiple', 'multiple');
+    }
+
+    componentDidMount() {
+        const url = '/api/sites';
+
+        try {
+            return axios.get(url).then(response => {
+                console.log(response);
+                this.setState({
+                    siteList: response.data.result,
+                    totalCount: response.data.result.length
+                })
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     handleFileSelect = (e) => {
@@ -73,7 +92,7 @@ class SiteList extends Component {
     }
 
     render() {
-        const { totalCount, addSiteModalOpen, modifySiteModalOpen, closeOnEscape, closeOnDimmerClick, modifiedCard } = this.state;
+        const { siteList, totalCount, addSiteModalOpen, modifySiteModalOpen, closeOnEscape, closeOnDimmerClick, modifiedCard } = this.state;
 
         return (
             <div style={{ marginTop: '4em', width: '70%', marginLeft: 'auto', marginRight: 'auto' }}>
@@ -107,79 +126,9 @@ class SiteList extends Component {
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column>
-                            <Card.Group centered>
-                                {/* Card #1 */}
-                                {/* {siteData.siteList.map((cardValue, index) => {
-                                    return (
-                                        <Card style={{ width: '300px', marginLeft: 'auto', marginRight: 'auto' }} key={index}>
-                                            <Image
-                                                style={{
-                                                    paddingTop: '5px',
-                                                    paddingBottom: '5px',
-                                                    paddingLeft: '5px',
-                                                    paddingRight: '5px'
-                                                }}
-                                                size='medium'
-                                                src={Gallery.getLogoImage(cardValue.name)}
-                                                as={Link}
-                                                to={{
-                                                    pathname: "/home/sites/sitedetails",
-                                                    state: [cardValue.name, cardValue.openDate, cardValue.numberOfServices]
-                                                }} />
-                                            <Card.Content>
-                                                <Grid columns={2} style={{ paddingTop: '-1em', paddingBottom: '-1em' }}>
-                                                    <Grid.Row
-                                                        style={{ paddingTop: '-1em', paddingBottom: '-1em' }}
-                                                    >
-                                                        <Grid.Column floated='left' verticalAlign='middle'>
-                                                            <Card.Header
-                                                                as={Link}
-                                                                to={{
-                                                                    pathname: "/home/sites/sitedetails",
-                                                                    state: [
-                                                                        cardValue.name,
-                                                                        cardValue.openDate,
-                                                                        cardValue.numberOfServices
-                                                                    ]
-                                                                }}>
-                                                                <Header as='h3'>{cardValue.name}</Header>
-                                                            </Card.Header>
-                                                        </Grid.Column>
-                                                        <Grid.Column floated='right' verticalAlign='middle'>
-                                                            <Menu secondary style={{ float: 'right' }}>
-                                                                <Dropdown item icon='ellipsis vertical' simple>
-                                                                    <Dropdown.Menu>
-                                                                        <Dropdown.Item
-                                                                            onClick={(v, e) => this.handleModifySiteMenuItem({
-                                                                                name: cardValue.name,
-                                                                                openDate: cardValue.openDate
-                                                                            }, e)}>Modify</Dropdown.Item>
-                                                                        <Dropdown.Item>Delete</Dropdown.Item>
-                                                                    </Dropdown.Menu>
-                                                                </Dropdown>
-                                                            </Menu>
-                                                        </Grid.Column>
-                                                    </Grid.Row>
-                                                </Grid>
-                                                <Card.Meta>{cardValue.openDate}</Card.Meta>
-                                            </Card.Content>
-                                            <Card.Content extra>
-                                                <Grid columns={2}>
-                                                    <Grid.Row>
-                                                        <Grid.Column floated='left' verticalAlign='middle' width={1} style={{ marginRight: '0em' }}>
-                                                            <Icon fitted name='setting' size='large' />
-                                                        </Grid.Column>
-                                                        <Grid.Column floated='left' verticalAlign='middle'>
-                                                            {cardValue.numberOfServices} Services
-                                                        </Grid.Column>
-                                                    </Grid.Row>
-                                                </Grid>
-                                            </Card.Content>
-                                        </Card>
-                                    );
-                                })} */}
+                            <Card.Group itemsPerRow='4'>
                                 {/* Card #2 */}
-                                {siteData.siteList.map((cardValue, index) => {
+                                {siteList.map((site, index) => {
                                     return (
                                         <Card style={{ width: '300px', height: '20vh', marginLeft: 'auto', marginRight: 'auto' }} key={index}>
                                             <Grid
@@ -194,15 +143,11 @@ class SiteList extends Component {
                                                     <Image
                                                         floated='left'
                                                         size='small'
-                                                        src={Gallery.getLogoImage(cardValue.name)}
+                                                        src={Gallery.getLogoImage(site.name)}
                                                         as={Link}
                                                         to={{
-                                                            pathname: "/home/sites/sitedetails",
-                                                            state: [
-                                                                cardValue.name,
-                                                                cardValue.openDate,
-                                                                cardValue.numberOfServices
-                                                            ]
+                                                            pathname: `/home/sites/sitedetails/${site.siteId}`,
+                                                            state: site.siteId
                                                         }}
                                                     />
                                                 </Grid.Column>
@@ -223,12 +168,12 @@ class SiteList extends Component {
                                                     to={{
                                                         pathname: "/home/sites/sitedetails",
                                                         state: [
-                                                            cardValue.name,
-                                                            cardValue.openDate,
-                                                            cardValue.numberOfServices
+                                                            site.name,
+                                                            site.openDate,
+                                                            site.countOfServices
                                                         ]
-                                                    }}>{cardValue.name}</Card.Header>
-                                                <Card.Meta>{cardValue.openDate}</Card.Meta>
+                                                    }}>{site.name}</Card.Header>
+                                                <Card.Meta>{site.openDate}</Card.Meta>
                                             </Card.Content>
                                             <Card.Content extra>
                                                 <Grid columns={2}>
@@ -237,7 +182,7 @@ class SiteList extends Component {
                                                             <Icon fitted name='setting' size='large' />
                                                         </Grid.Column>
                                                         <Grid.Column floated='left' verticalAlign='middle'>
-                                                            {cardValue.numberOfServices} Services
+                                                            {site.countOfServices} Services
                                                         </Grid.Column>
                                                     </Grid.Row>
                                                 </Grid>
@@ -245,65 +190,6 @@ class SiteList extends Component {
                                         </Card>
                                     );
                                 })}
-                                {/* Card #3 */}
-                                {/* {siteData.siteList.map((cardValue, index) => {
-                                    return (
-                                        <Card style={{ width: '300px', height: '20vh', marginLeft: 'auto', marginRight: 'auto' }} key={index}>
-                                            <Grid columns={3} style={{ paddingTop: '5px', paddingBottom: '5px', paddingLeft: '5px', paddingRight: '5px' }}>
-                                                <Grid.Column floated='left' verticalAlign='middle'>
-                                                    <Header as='h3' style={{
-                                                        paddingTop: '5px',
-                                                        paddingBottom: '5px',
-                                                        paddingLeft: '5px',
-                                                        paddingRight: '5px'
-                                                    }}>{cardValue.name}</Header>
-                                                </Grid.Column>
-                                                <Grid.Column floated='left' verticalAlign='middle'>
-                                                    <Image
-                                                        style={{ marginLeft: '-2em' }}
-                                                        floated='left'
-                                                        size='large'
-                                                        src={Gallery.getLogoImage(cardValue.name)}
-                                                        as={Link}
-                                                        to={{
-                                                            pathname: "/home/sites/sitedetails",
-                                                            state: [
-                                                                cardValue.name,
-                                                                cardValue.openDate,
-                                                                cardValue.numberOfServices
-                                                            ]
-                                                        }}
-                                                    />
-                                                </Grid.Column>
-                                                <Grid.Column floated='right' verticalAlign='middle'>
-                                                    <Menu secondary style={{ float: 'right' }}>
-                                                        <Dropdown item icon='ellipsis vertical' simple>
-                                                            <Dropdown.Menu>
-                                                                <Dropdown.Item>Modify</Dropdown.Item>
-                                                                <Dropdown.Item>Delete</Dropdown.Item>
-                                                            </Dropdown.Menu>
-                                                        </Dropdown>
-                                                    </Menu>
-                                                </Grid.Column>
-                                            </Grid>
-                                            <Card.Content>
-                                                <Card.Meta>{cardValue.openDate}</Card.Meta>
-                                            </Card.Content>
-                                            <Card.Content extra>
-                                                <Grid columns={2}>
-                                                    <Grid.Row>
-                                                        <Grid.Column floated='left' verticalAlign='middle' width={1} style={{ marginRight: '0em' }}>
-                                                            <Icon fitted name='setting' size='large' />
-                                                        </Grid.Column>
-                                                        <Grid.Column floated='left' verticalAlign='middle'>
-                                                            {cardValue.numberOfServices} Services
-                                                        </Grid.Column>
-                                                    </Grid.Row>
-                                                </Grid>
-                                            </Card.Content>
-                                        </Card>
-                                    );
-                                })} */}
                             </Card.Group>
                         </Grid.Column>
                     </Grid.Row>
@@ -357,7 +243,7 @@ class SiteList extends Component {
                                 <Form.Input fluid label='Site name' value={modifiedCard.name} />
                             </Form.Group>
                             <Form.Group widths='equal'>
-                                <SemanticDatepicker label='Open date' datePickerOnly={true} onChange={this.onChange} value={modifiedCard.openDate}/>
+                                <SemanticDatepicker label='Open date' datePickerOnly={true} onChange={this.onChange} value={modifiedCard.openDate} />
                             </Form.Group>
                             <Form.Group widths='equal'>
                                 <Form.Input
