411d9953750f9f283e6430e63b7fd85caacb4979
myhency <hency.yeo@gmail.com>
2020-03-11 15:28:12 +0900

    feat - add link that from instance list to instance detail

--- src/pages/InstanceList.js
+++ src/pages/InstanceList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, Segment, Header, Modal, Grid, Icon, Select, Divider } from 'semantic-ui-react';
+import { Button, Form, Segment, Header, Modal, Grid, Icon, Select, Divider, Table, Menu } from 'semantic-ui-react';
 import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
 import ListTable from '../components/ListTable';
 import SiteData from '../assets/data/SiteData.json';
@@ -8,11 +8,38 @@ import InstanceData from '../assets/data/InstanceData.json';
 import RoleData from '../assets/data/RoleData.json';
 import InstanceStatusData from '../assets/data/InstanceStatusData.json';
 import PageTitle from '../components/PageTitle';
+import axios from 'axios';
 
 const headers = ['Site name', 'Service name', 'Instance name', 'Endpoint', 'Status'];
 
+const EmptyColumns = (length) => {
+    let rows = [];
+    let columnLength = length;
+
+    for (let i = columnLength; i < 9; i++) {
+        rows.push((
+            <Table.Row key={i}>
+                <EmptyCells data={columnLength} />
+            </Table.Row>
+        ))
+    }
+    return rows;
+}
+
+const EmptyCells = (length) => {
+    let cells = [];
+    let cellDataLength = length;
+
+    for (let i = 0; i < cellDataLength; i++) {
+        cells.push((
+            <Table.Cell style={{ fontSize: '16px' }} textAlign='center' key={i}>&nbsp;</Table.Cell>
+        ))
+    }
+    return cells;
+}
+
 class InstanceList extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
@@ -20,28 +47,64 @@ class InstanceList extends Component {
             open: false,
             totalCount: 4,
             addServiceModalOpen: false,
-            data: {
-                cellData: []
-            },
             siteOption: [],
-            serviceOption: []
+            serviceOption: [],
+            instanceData: [{
+                instanceId: 0,
+                data: [['','','','','']]
+            }],
         };
 
-    }
-    
-    static getDerivedStateFromProps(props, state) {
-        console.log(InstanceData);
-        
-        let { data, siteOption, serviceOption } = state;
+        // this.getInstanceList();
 
+    }
+
+    componentDidMount() {
+        this.getInstanceList();
+    }
+
+    getInstanceList = () => {
         // set instance list table data
-        data.cellData.splice(0, data.cellData.length);
-        Array.prototype.forEach.call(InstanceData.instanceList, value => {
-            let arr = [];
-            arr.push(value.siteName, value.serviceName, value.name, value.endPoint, value.status);
-            data.cellData.push(arr);
+        // data.cellData.splice(0, data.cellData.length);
+        let instanceData = [{}];
+
+        Array.prototype.forEach.call(InstanceData.instanceList, instance => {
+            instanceData.push({
+                instanceId: instance.instanceId,
+                data: [
+                    instance.siteName,
+                    instance.serviceName,
+                    instance.name,
+                    instance.endPoint,
+                    instance.status
+                ]
+            });
         });
-        
+
+        this.setState({ instanceData });
+
+        // return InstanceStatusData;
+        // const url = `/api/instances/${id}`;
+
+        // try {
+        //     return axios.get(url).then(response => {
+        //         console.log(response);
+        //         this.setState({
+        //             siteName: response.data.result.siteName,
+        //             serviceName: response.data.result.serviceName,
+        //             instanceName: response.data.result.instanceName,
+        //             endPoint: response.data.result.endpoint,
+        //             status: response.data.result.status.toString()
+        //         })
+        //     });
+        // } catch (error) {
+        //     console.log(error);
+        // }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        let { siteOption, serviceOption } = state;
+
         // set site name search condition
         siteOption.splice(0, siteOption.length);
         Array.prototype.forEach.call(SiteData.siteList, value => {
@@ -73,7 +136,7 @@ class InstanceList extends Component {
         })
 
         return {
-            ...data, ...siteOption, ...serviceOption
+            ...siteOption, ...serviceOption
         }
     }
 
@@ -85,10 +148,10 @@ class InstanceList extends Component {
 
     close = () => this.setState({ open: false });
 
-    handleClick = rowValue => {
+    handleClick = id => {
         this.props.history.push({
-            pathname: '/home/instances/instancedetails/',
-            state: rowValue
+            pathname: `/home/instances/instancedetails/${id}`,
+            state: id
         });
     }
 
@@ -99,9 +162,9 @@ class InstanceList extends Component {
     addInstanceModalClose = () => this.setState({ addServiceModalOpen: false });
 
     render() {
-        const { data, siteOption, serviceOption, addServiceModalOpen, closeOnEscape, closeOnDimmerClick } = this.state;
-        console.log(siteOption);
-        console.log(RoleData.roles);
+        const { instanceData, siteOption, serviceOption, addServiceModalOpen, closeOnEscape, closeOnDimmerClick } = this.state;
+        instanceData.splice(0,1);
+
         return (
             <div style={{ marginTop: '4em', width: '70%', marginLeft: 'auto', marginRight: 'auto' }}>
                 <Grid>
@@ -109,7 +172,7 @@ class InstanceList extends Component {
                         <Grid.Column>
                             <PageTitle
                                 title='Instances'
-                                description='Autoever DID hub 에 등록된 모든 Instance들을 보여줍니다. Instance는 Host VM의 Docker container 단위를 의미합니다.' 
+                                description='Autoever DID hub 에 등록된 모든 Instance들을 보여줍니다. Instance는 Host VM의 Docker container 단위를 의미합니다.'
                                 iconName='server'
                             />
                         </Grid.Column>
@@ -159,10 +222,60 @@ class InstanceList extends Component {
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column>
-                            <ListTable
-                                handleClick={(rowValue) => this.handleClick(rowValue)}
-                                headers={headers}
-                                data={data} />
+                            <Table celled selectable>
+                                <Table.Header>
+                                    <Table.Row>
+                                        {headers.map((value, index) => {
+                                            return <Table.HeaderCell
+                                                style={{
+                                                    fontSize: '20px',
+                                                    backgroundColor: 'Gainsboro'
+                                                }}
+                                                textAlign='center'
+                                                key={index}>{value}
+                                            </Table.HeaderCell>
+                                        })}
+                                    </Table.Row>
+                                </Table.Header>
+
+                                <Table.Body>
+                                    {instanceData.map((rowValue, rowIndex) => {
+                                        return (
+                                            <Table.Row
+                                                key={rowIndex}
+                                                onClick={() => this.handleClick(rowValue.instanceId)}>
+                                                {rowValue.data.map((cellValue, cellIndex) => {
+                                                    return (
+                                                        <Table.Cell
+                                                            style={{ fontSize: '16px' }}
+                                                            textAlign='center'
+                                                            key={cellIndex}>{cellValue}
+                                                        </Table.Cell>)
+                                                })}
+                                            </Table.Row>
+                                        );
+                                    })}
+                                    <EmptyColumns data={headers.length} />
+                                </Table.Body>
+                                <Table.Footer>
+                                    <Table.Row>
+                                        <Table.HeaderCell colSpan='5'>
+                                            <Menu floated='right' pagination>
+                                                <Menu.Item as='a' icon>
+                                                    <Icon name='chevron left' />
+                                                </Menu.Item>
+                                                <Menu.Item as='a'>1</Menu.Item>
+                                                <Menu.Item as='a'>2</Menu.Item>
+                                                <Menu.Item as='a'>3</Menu.Item>
+                                                <Menu.Item as='a'>4</Menu.Item>
+                                                <Menu.Item as='a' icon>
+                                                    <Icon name='chevron right' />
+                                                </Menu.Item>
+                                            </Menu>
+                                        </Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Footer>
+                            </Table>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
@@ -194,7 +307,7 @@ class InstanceList extends Component {
                                 />
                             </Form.Group>
                             <Form.Group widths='equal'>
-                                <Form.Input fluid label='Instance name' placeholder='service name + instance name + #x'/>
+                                <Form.Input fluid label='Instance name' placeholder='service name + instance name + #x' />
                             </Form.Group>
                             <Form.Group widths='equal'>
                                 <Form.Input fluid label='Endpoint' placeholder='https://example.com/' />
