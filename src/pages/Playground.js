0a0a2fd7c906ef6ca2e2edb8ad1fe3e26ed3ce7d
myhency <hency.yeo@gmail.com>
2020-04-12 10:39:52 +0900

    add components

--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
-import { JamesCard, JamesGrid, JamesRow, JamesColumn, JamesButton } from '../themes/jamesStyledComponents';
+import {
+    JamesCard,
+    JamesGrid,
+    JamesRow,
+    JamesColumn,
+    JamesButton,
+    JamesHeader,
+    JamesTable,
+    JamesTableHeader,
+    JamesTableRow,
+    JamesTableBody,
+    JamesTableHeaderCell,
+    JamesTableCell,
+    JamesLineGraph,
+    JamesBarGraph
+
+} from '../themes/jamesStyledComponents';
 
 
 class Playground extends Component {
@@ -8,11 +24,90 @@ class Playground extends Component {
             <JamesGrid celled>
                 <JamesRow columns={2}>
                     <JamesColumn computer={8} tablet={16} mobile={16}>
-                        <JamesCard>aaaa</JamesCard>
+                        JamesHeader Sample
                     </JamesColumn>
                     <JamesColumn computer={8} tablet={16} mobile={16}>
-                        <JamesCard>aaaa</JamesCard>
-                        <JamesButton color='black' />
+                        <JamesHeader>JamesHeader Sample</JamesHeader>
+                    </JamesColumn>
+                </JamesRow>
+                <JamesRow columns={2}>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        JamesCard Sample
+                    </JamesColumn>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        <JamesCard>JamesCard Sample</JamesCard>
+                    </JamesColumn>
+                </JamesRow>
+                <JamesRow columns={2}>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        JamesTable Sample
+                    </JamesColumn>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        <JamesTable>
+                            <JamesTableHeader>
+                                <JamesTableRow>
+                                    <JamesTableHeaderCell>
+                                        Column1
+                                    </JamesTableHeaderCell>
+                                    <JamesTableHeaderCell>
+                                        Column2
+                                    </JamesTableHeaderCell>
+                                    <JamesTableHeaderCell>
+                                        Column3
+                                    </JamesTableHeaderCell>
+                                </JamesTableRow>
+                            </JamesTableHeader>
+                            <JamesTableBody>
+                                <JamesTableRow>
+                                    <JamesTableCell>
+                                        1
+                                    </JamesTableCell>
+                                    <JamesTableCell>
+                                        2
+                                    </JamesTableCell>
+                                    <JamesTableCell>
+                                        3
+                                    </JamesTableCell>
+                                </JamesTableRow>
+                                <JamesTableRow>
+                                    <JamesTableCell>
+                                        4
+                                    </JamesTableCell>
+                                    <JamesTableCell>
+                                        5
+                                    </JamesTableCell>
+                                    <JamesTableCell>
+                                        6
+                                    </JamesTableCell>
+                                </JamesTableRow>
+                            </JamesTableBody>
+                        </JamesTable>
+                    </JamesColumn>
+                </JamesRow>
+                <JamesRow columns={2}>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        JamesLineGraph Sample
+                    </JamesColumn>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        <JamesCard>
+                            <JamesLineGraph data={{
+                                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+                                values: [10, 20, 30, 100, 15, 10, 20, 30, 10, 150]
+                            }} />
+                        </JamesCard>
+                    </JamesColumn>
+                </JamesRow>
+                <JamesRow columns={2}>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        JamesBarGraph Sample
+                    </JamesColumn>
+                    <JamesColumn computer={8} tablet={16} mobile={16}>
+                        <JamesCard>
+                            <JamesBarGraph data={{
+                                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+                                values: [10, 20, 30, 100, 15, 10, 20, 30, 10, 150]
+                            }} />
+                        </JamesCard>
                     </JamesColumn>
                 </JamesRow>
             </JamesGrid>
--- src/themes/jamesStyledComponents.js
+++ src/themes/jamesStyledComponents.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Grid, Button } from 'semantic-ui-react';
+import { Container, Grid, Button, Table } from 'semantic-ui-react';
 import styled from 'styled-components';
+import { Line, Bar } from 'react-chartjs-2';
 
 export const JamesCard = styled(Container)({
     backgroundColor: 'white',
@@ -24,3 +25,221 @@ export const JamesColumn = styled(Grid.Column)({
 export const JamesButton = styled(Button)({
     backgroundColor: props => props.color
 });
+
+export const JamesHeader = styled.span({
+    width: '83px',
+    height: '27px',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: '1.5',
+    letterSpacing: '-0.18px',
+    textAlign: 'left',
+    color: '#3b4a5f'
+});
+
+export const JamesTable = styled(Table)({
+    borderWidth: '0px!important'
+});
+
+export const JamesTableHeader = styled(Table.Header)({
+});
+
+export const JamesTableRow = styled(Table.Row)({
+});
+
+export const JamesTableBody = styled(Table.Body)({
+});
+
+export const JamesTableHeaderCell = styled(Table.HeaderCell)({
+    fontSize: '13px!important',
+    color: '#8391a5!important',
+    backgroundColor: '#f7f8fb!important',
+    padding: '7px 0px 7px 16px!important'
+});
+
+export const JamesTableCell = styled(Table.Cell)({
+    padding: '9px 0px 9px 16px!important',
+    fontSize: '16px!important',
+    color: '#3b4a5f!important'
+});
+
+export class JamesLineGraph extends React.Component {
+    render() {
+        const options = {
+            tooltips: {
+                titleFontSize: 16,
+                bodyFontSize: 18,
+                // rtl: false,
+                backgroundColor: 'rgb(47,61,80)',
+                titleAlign: 'right',
+                bodyAlign: 'right'
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        zeroLineWidth: 0,
+                        drawBorder: false,
+                        color: '#f1f2f5',
+                        display: true
+                    },
+                    ticks: {
+                        labelOffset: 15,
+                        fontColor: '#8391a5',
+                        fontSize: 13,
+                        autoSkipPadding: 80,
+                        maxRotation: 0,
+                        padding: 5,
+                        // max: 'now'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        fontColor: '#8391a5',
+                        fontSize: 13,
+                        beginAtZero: true,
+                        stepSize: 20,
+                        suggestedMax: 100
+                    }
+                }]
+            },
+            maintainAspectRatio: false,
+        }
+
+        let graphData = {
+            labels: this.props.data.labels,
+            datasets: [
+                {
+                    label: 'Sample Data',
+                    fill: true,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgb(66,128,245,0.15)',
+                    borderColor: '#4280f5',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderWidth: 1,
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: '#4280f5',
+                    pointBackgroundColor: '#4280f5',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: '#4280f5',
+                    pointHoverBorderColor: '#4280f5',
+                    pointHoverBorderWidth: 1,
+                    pointRadius: 0,
+                    pointHitRadius: 3,
+                    data: this.props.data.values
+                }
+            ]
+        }
+
+        return (
+            <Line
+                data={graphData}
+                options={options}
+            />
+        );
+    }
+}
+
+export class JamesBarGraph extends React.Component {
+    
+
+    render() {
+
+        const options = {
+            tooltips: {
+                titleFontSize: 16,
+                bodyFontSize: 18,
+                rtl: false,
+                backgroundColor: 'rgb(47,61,80)',
+                titleAlign: 'right',
+                bodyAlign: 'right'
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        zeroLineWidth: 0,
+                        drawBorder: false,
+                        color: '#f1f2f5',
+                        display: true
+                    },
+                    ticks: {
+                        labelOffset: 15,
+                        fontColor: '#8391a5',
+                        fontSize: 13,
+                        autoSkipPadding: 80,
+                        maxRotation: 0,
+                        padding: 5,
+                        max: 'now'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        fontColor: '#8391a5',
+                        fontSize: 13,
+                        beginAtZero: true,
+                        stepSize: 1,
+                        suggestedMax: 5
+                    }
+                }]
+            },
+            maintainAspectRatio: false,
+        }
+
+        let graphData = {
+            labels: this.props.data.labels,
+            datasets: [
+                {
+                    label: 'Sample Data',
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: '#9adbff',
+                    borderColor: '#9adbff',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    barThickness: 6,
+                    categoryPercentage: 1,
+                    barPercentage: 0.5,
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: '#9adbff',
+                    pointBackgroundColor: '#9adbff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: '#9adbff',
+                    pointHoverBorderColor: '#9adbff',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: this.props.data.values
+                }
+            ]
+        }
+
+        return (
+            <Bar
+                data={graphData}
+                options={options}
+            />
+        );
+    }
+}
